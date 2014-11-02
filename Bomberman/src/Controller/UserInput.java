692bea660a788df4f6527bc86fe6bda74288423a
sangmoonhwang <ghkdtkdans@gmail.com>
2014-11-02 02:46:33 -0500

    Collision with indestructible /HSM

--- Bomberman/src/Controller/UserInput.java
+++ Bomberman/src/Controller/UserInput.java
@@ -5,109 +5,111 @@ package Controller;
 
 import java.awt.event.*;
 
+import Model.Indestructible;
 import View.DrawMap;
 
 public class UserInput implements KeyListener, FocusListener{
-    private DrawMap d;
-    private float xVel = 0;
-    private float yVel = 0;
+	private DrawMap d;
+	//private Indestructible[] idstr = d.getindestructible();
+	private float xVel = 0;
+	private float yVel = 0;
 	private static boolean running = false;
 	public UserInput(){
 		d = new DrawMap();
 	}
-	
+
 	public static void main(String[] args){
-        UserInput test = new UserInput();
-        running = true;
-        test.run();
-    }
-	
+		UserInput test = new UserInput();
+		running = true;
+		test.run();
+	}
+
 	public void run(){
 		d.run();
 		d.getCanvas().addFocusListener(this);
-        d.getCanvas().addKeyListener(this);
-        d.getCanvas().requestFocus();
-        
-        long start = System.nanoTime();
-        final double amountOfTicks = 60.0;
-        double ns = 1000000000 / amountOfTicks;
-        double delta = 0;
-        
-        while(running) {
-        	long now = System.nanoTime();
-        	delta += (now - start) / ns;
-        	start = now;
-        	
-        	if(delta >= 1) {
-        		tick();
-        		delta--;
-        	}
-        	d.update();
-        	
-        }
+		d.getCanvas().addKeyListener(this);
+		d.getCanvas().requestFocus();
+
+		long start = System.nanoTime();
+		final double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - start) / ns;
+			start = now;
+
+			if(delta >= 1) {
+				tick();
+				delta--;
+			}
+			d.update();
+
+		}
 	}
-	
+
 	public void keyPressed ( KeyEvent e ){
-        int value = e.getKeyCode();
-        if (value == KeyEvent.VK_DOWN && value !=KeyEvent.VK_UP){
-        	setVelY(2);
-        }
-        else if(value != KeyEvent.VK_DOWN && value ==KeyEvent.VK_UP){
-        	setVelY(-2);
-        }
-        else if(value == KeyEvent.VK_LEFT && value !=KeyEvent.VK_RIGHT){
-        	setVelX(-2);
-        }
-        else if(value == KeyEvent.VK_RIGHT && value !=KeyEvent.VK_LEFT){
-        	setVelX(2);
-        }
-//        switch(value){
-//            case KeyEvent.VK_DOWN:setVelY(1);
-//            break;
-//            case KeyEvent.VK_UP:setVelY(-1);
-//            break;
-//            case KeyEvent.VK_RIGHT:setVelX(1);
-//            break;
-//            case KeyEvent.VK_LEFT:setVelX(-1);
-//            break;
-//        }
-   }
-	
+		int value = e.getKeyCode();
+		if (value == KeyEvent.VK_DOWN && value !=KeyEvent.VK_UP){
+			setVelY(2);
+		}
+		else if(value != KeyEvent.VK_DOWN && value ==KeyEvent.VK_UP){
+			setVelY(-2);
+		}
+		else if(value == KeyEvent.VK_LEFT && value !=KeyEvent.VK_RIGHT){
+			setVelX(-2);
+		}
+		else if(value == KeyEvent.VK_RIGHT && value !=KeyEvent.VK_LEFT){
+			setVelX(2);
+		}
+		//        switch(value){
+		//            case KeyEvent.VK_DOWN:setVelY(1);
+		//            break;
+		//            case KeyEvent.VK_UP:setVelY(-1);
+		//            break;
+		//            case KeyEvent.VK_RIGHT:setVelX(1);
+		//            break;
+		//            case KeyEvent.VK_LEFT:setVelX(-1);
+		//            break;
+		//        }
+	}
+
 	public void keyTyped(KeyEvent e) {
 	}
 
 	public void keyReleased(KeyEvent e) {
-        int value = e.getKeyCode();
-        if (value == KeyEvent.VK_DOWN){
-        	if(yVel == 2)
-        	setVelY(0);
-        }
-        else if(value ==KeyEvent.VK_UP){
-        	if(yVel == -2)
-        	setVelY(0);
-        }
-        else if(value == KeyEvent.VK_LEFT){
-        	if(xVel == -2)
-        	setVelX(0);
-        }
-        else if(value == KeyEvent.VK_RIGHT){
-        	if(xVel ==2)
-        	setVelX(0);
-        }
-        else{
-        	setVelX(0);
-        	setVelY(0);
-        }
-//        switch(value){
-//            case KeyEvent.VK_DOWN:setVelY(0);
-//            break;
-//            case KeyEvent.VK_UP:setVelY(0);
-//            break;
-//            case KeyEvent.VK_RIGHT:setVelX(0);
-//            break;
-//            case KeyEvent.VK_LEFT:setVelX(0);
-//            break;
-//        }
+		int value = e.getKeyCode();
+		if (value == KeyEvent.VK_DOWN){
+			if(yVel == 2)
+				setVelY(0);
+		}
+		else if(value ==KeyEvent.VK_UP){
+			if(yVel == -2)
+				setVelY(0);
+		}
+		else if(value == KeyEvent.VK_LEFT){
+			if(xVel == -2)
+				setVelX(0);
+		}
+		else if(value == KeyEvent.VK_RIGHT){
+			if(xVel ==2)
+				setVelX(0);
+		}
+		else{
+			setVelX(0);
+			setVelY(0);
+		}
+		//        switch(value){
+		//            case KeyEvent.VK_DOWN:setVelY(0);
+		//            break;
+		//            case KeyEvent.VK_UP:setVelY(0);
+		//            break;
+		//            case KeyEvent.VK_RIGHT:setVelX(0);
+		//            break;
+		//            case KeyEvent.VK_LEFT:setVelX(0);
+		//            break;
+		//        }
 	}    
 
 	public void focusGained(FocusEvent e) {
@@ -115,18 +117,25 @@ public class UserInput implements KeyListener, FocusListener{
 
 	public void focusLost(FocusEvent e) {
 	}
-	
+
 	public void tick() {
 		d.getBomberman().setXval(xVel);
 		d.getBomberman().setYval(yVel);
+		for (int i = 0; i < 100; i++){
+			if(d.getBomberman().getXval()+35 > d.indestructibles[i].getXval()*50 && d.getBomberman().getYval() < d.indestructibles[i].getYval()*50+35 &&
+					d.getBomberman().getXval() < d.indestructibles[i].getXval()*50+35 && d.getBomberman().getYval()+35 > d.indestructibles[i].getYval()*50){
+				d.getBomberman().setXval(-xVel);
+				d.getBomberman().setYval(-yVel);
+			}
+		}
 	}
-	
+
 	public void setVelX(float xVel) {
 		this.xVel = xVel;
 	}
-	
+
 	public void setVelY(float yVel) {
 		this.yVel = yVel;
 	}
-	
+
 }
--- Bomberman/src/View/DrawMap.java
+++ Bomberman/src/View/DrawMap.java
@@ -16,85 +16,90 @@ import Model.Indestructible;
 
 
 public class DrawMap extends JComponent{
- private JFrame gameFrame;
-    private DrawingArea canvas;
-    private Bomberman bombman;
-    private Indestructible[] indestructibles;
-    private Image bombermanSprite;
- 
- public DrawMap(){
-   gameFrame = new JFrame("Bomberman");
-   bombman = new Bomberman();
-   bombermanSprite = Toolkit.getDefaultToolkit().getImage("Bomberman.GIF");
-   indestructibles = new Indestructible[101];
-   for (int i = 0; i < 100; i++){
-     indestructibles[i] = new Indestructible();
-   }
- }
- 
- public void run(){
-        canvas = new DrawingArea();
-        makeFrame();
-        gameFrame.getContentPane().add(canvas);
-        gameFrame.setVisible(true);
-    }
- 
- public void makeFrame(){
-  gameFrame.setSize(751,673);
-  gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-  Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-  gameFrame.setLocation(dim.width/2-gameFrame.getSize().width/2, dim.height/2-gameFrame.getSize().height/2);
-  
-  gameFrame.getContentPane().add(new DrawMap());
-  gameFrame.setVisible(true);
- }
- 
- public class DrawingArea extends JPanel{ //what does this do
-  public void paintComponent(Graphics g){
-            super.paintComponent(g);
-            int width = 50;
-            int height = 50;
-      for(int x=0; x<15; x++){
-          for(int y=0; y<13; y++){
-              /*if( (x == 0 || y == 0 || y == 12) || (x%2 == 0 && y%2 == 0)){
+	private JFrame gameFrame;
+	private DrawingArea canvas;
+	private Bomberman bombman;
+	public Indestructible[] indestructibles;
+	private Image bombermanSprite;
+
+	public DrawMap(){
+		gameFrame = new JFrame("Bomberman");
+		bombman = new Bomberman();
+		bombermanSprite = Toolkit.getDefaultToolkit().getImage("Bomberman.GIF");
+		indestructibles = new Indestructible[101];
+		for (int i = 0; i < 100; i++){
+			indestructibles[i] = new Indestructible();
+		}
+	}
+
+	public void run(){
+		canvas = new DrawingArea();
+		makeFrame();
+		gameFrame.getContentPane().add(canvas);
+		gameFrame.setVisible(true);
+	}
+
+	public void makeFrame(){
+		gameFrame.setSize(751,673);
+		gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		gameFrame.setLocation(dim.width/2-gameFrame.getSize().width/2, dim.height/2-gameFrame.getSize().height/2);
+
+		gameFrame.getContentPane().add(new DrawMap());
+		gameFrame.setVisible(true);
+	}
+
+	public class DrawingArea extends JPanel{ //what does this do
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			int width = 50;
+			int height = 50;
+			for(int x=0; x<15; x++){
+				for(int y=0; y<13; y++){
+					/*if( (x == 0 || y == 0 || y == 12) || (x%2 == 0 && y%2 == 0)){
                g.setColor(Color.GRAY);
                g.fillRect(x*width,y*height,width,height);
               }*/
-              g.setColor(Color.BLACK);
-              g.drawRect(x*width,y*height,width,height);
-          }
-      }
-      int i = 0;
-      for(int x=0; x<15; x++){
-        for(int y=0; y<13; y++){
-          if( (x == 0 || y == 0 || y == 12) || (x%2 == 0 && y%2 == 0)){
-            indestructibles[i].setYval(y);
-            indestructibles[i].setXval(x);
-            g.setColor(Color.GRAY);
-            g.fillRect(indestructibles[i].getXval()*width,indestructibles[i].getYval()*height,width,height);
-            i++;
-          }
-        }
-      }
-  
-      
-      
-            g.setColor(Color.ORANGE);
-            //g.fillRect((int)bombman.getXval(), (int)bombman.getYval(), 40, 50); //change to rocket
-            g.drawImage(bombermanSprite,(int)bombman.getXval(),(int)bombman.getYval(),50,50,this);
-        }
-  
- }
- 
- public Bomberman getBomberman(){
-  return bombman;
- }
- 
- public DrawingArea getCanvas(){
-  return canvas;
- }
- 
- public void update(){
-  canvas.repaint();
- }
+					g.setColor(Color.BLACK);
+					g.drawRect(x*width,y*height,width,height);
+				}
+			}
+			int i = 0;
+			for(int x=0; x<15; x++){
+				for(int y=0; y<13; y++){
+					if( (x == 0 || y == 0 || y == 12) || (x%2 == 0 && y%2 == 0)){
+						indestructibles[i].setYval(y);
+						indestructibles[i].setXval(x);
+						g.setColor(Color.GRAY);
+						g.fillRect(indestructibles[i].getXval()*width,indestructibles[i].getYval()*height,width,height);
+						i++;
+					}
+				}
+			}
+
+
+
+			g.setColor(Color.ORANGE);
+			//g.fillRect((int)bombman.getXval(), (int)bombman.getYval(), 40, 50); //change to rocket
+			g.drawImage(bombermanSprite,(int)bombman.getXval(),(int)bombman.getYval(),50,50,this);
+		}
+
+	}
+
+	public Bomberman getBomberman(){
+		return bombman;
+	}
+
+	public DrawingArea getCanvas(){
+		return canvas;
+	}
+
+	public void update(){
+		canvas.repaint();
+	}
+
+	//public Indestructible[] getindestructible() {
+	//	// TODO Auto-generated method stub
+	//	return this.indestructibles;
+	//}
 }
