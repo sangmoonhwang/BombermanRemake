1cf1fc9b8c449e916ec59c899b9e94b1b5f8f845
unknown <alexander.makrigiorgos@mail.mcgill.ca>
2014-11-08 20:44:51 -0500

    drew the login window, text fields and login button, main method is in Login.java

--- Bomberman/src/Controller/Login.java
+++ Bomberman/src/Controller/Login.java
@@ -1,19 +1,35 @@
 package Controller;
 
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
 import Model.User;
 import Model.Database;
+import View.DrawLogin;
 
-public class Login extends Database {
+public class Login extends Database implements KeyListener, FocusListener {
+	private DrawLogin d;
+	private static boolean running = false;
 
 	CreateAccount newUser;
 	ModifyAccount modifyUser;
 	
 	//draw login with button listener
 	public Login() {
-		
+		d = new DrawLogin();
 	}
+	
+	public static void main(String[] args){
+		Login test = new Login();
+		running = true;
+		test.run();
+	}
+	 public void run(){
+		  d.run();
+	 }
 	
 	//if user exists then login to menu else display user does not exist
 	public void loginUser(String username, String password) {
@@ -42,5 +58,35 @@ public class Login extends Database {
 	//call modifyAccount Class
 	public void modifyUser() {
 		modifyUser = new ModifyAccount();
+	}
+
+	@Override
+	public void focusGained(FocusEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void focusLost(FocusEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- Bomberman/src/View/DrawLogin.java
+++ Bomberman/src/View/DrawLogin.java
@@ -1,5 +1,109 @@
+//help from http://www.tutorialspoint.com/swing/swing_jtextfield.htm
+
 package View;
 
-public class DrawLogin {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.WindowConstants;
+import javax.swing.JTextField;
+
+
+
+
+
+
+public class DrawLogin extends JFrame{
+ private JFrame mainFrame;
+ private JLabel headerLabel;
+ private JLabel statusLabel;
+ private JPanel controlPanel;
+
+ public DrawLogin(){
+  //prepareGui();
+  mainFrame = new JFrame("Login");
+  
+  
+  //created arrays of tiles/indestructibles/bomberman
+  
+ }
+
+ public void prepareGui() {
+	mainFrame = new JFrame("Login");
+	mainFrame.setSize(1000, 1000);
+	mainFrame.setLayout(new GridLayout(3,1));
+	mainFrame.addWindowListener(new WindowAdapter() {
+		public void windowClosing(WindowEvent windowEvent){
+			System.exit(0);
+		}
+	});
+	headerLabel = new JLabel("",JLabel.CENTER);
+	statusLabel = new JLabel("",JLabel.CENTER);
+	
+	statusLabel.setSize(350,100);
+	
+	controlPanel = new JPanel();
+	controlPanel.setLayout(new FlowLayout());
+	
+	mainFrame.add(headerLabel);
+	mainFrame.add(controlPanel);
+	mainFrame.add(statusLabel);
+	mainFrame.setVisible(true);
+	
+}
+ 
+
+
+ public void run(){
+	 prepareGui();
+	 showLogin();
+	 //mainFrame.setVisible(true);
+ 	}
+ private void showLogin(){
+	 headerLabel.setText("Enter your Username and Password");
+	 
+	 JLabel namelabel = new JLabel("Username: ", JLabel.RIGHT);
+	 JLabel passwordLabel = new JLabel("Password: ", JLabel.CENTER);
+	 final JTextField userText = new JTextField(6);
+	 final JPasswordField passwordText = new JPasswordField(6);
+	 
+	 JButton verifyButton = new JButton("Login");
+	 verifyButton.addActionListener(new ActionListener() {
+		 public void actionPerformed(ActionEvent e) {
+			 String data = "Username " + userText.getText();
+			 data += ", Password: " + new String(passwordText.getPassword());
+			 statusLabel.setText(data);
+		 }
+	 });
+	 
+	 controlPanel.add(namelabel);
+	 controlPanel.add(userText);
+	 controlPanel.add(passwordLabel);
+	 controlPanel.add(passwordText);
+	 controlPanel.add(verifyButton);
+	 mainFrame.setVisible(true);
+ }
+
+
+
+
+
+
+
 
 }
